3397d4c8f4035cdfd7b15a11278babd693718ea8
HKdenis <kayanjadenish@gmail.com>
2025-10-25 18:04:22 +0300

    Initial commit of Streamlit app

---
+++ technical_assitance_emtct.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+title= st.title("Technical Assistance Tool")
+st.markdown("""
+MWP EMTCT Team""")
+            
